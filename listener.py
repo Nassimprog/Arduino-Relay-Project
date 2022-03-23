58fb80960432b3e38dc9048e238f80adc825b8ac
Nassimprog <rhaiemn@coventry.ac.uk>
2022-03-23 21:49:33 +0000

    added gitignore

--- listener.py
+++ listener.py
@@ -1,12 +1,30 @@
 import serial
 import time
-    
-with serial.Serial('COM10', 9600, timeout=10) as ser:
-    #x = ser.read()          # read one 
-    while True: # while not found device
-        #s = ser.read(1024)        # read up to ten bytes (timeout)
-        line = ser.readline()   # read a '\n' terminated line
-        print(line)
-        time.sleep(0.01)
 
+def serialReader():
+    with serial.Serial('COM10', 9600, timeout=10) as read_ser:
+        #x = read_ser.read()          # read one 
+        while True: # while not found device
+            #s = read_ser.read(1024)        # read up to ten bytes (timeout)
+            line = read_ser.readline()   # read a '\n' terminated line
+            print(line)
+            time.sleep(0.01)
+        #read_ser.close()
 
+def serialWriter():
+    with serial.Serial('COM9', 9600, timeout=10) as write_ser:
+        #print("secret message")
+        write_ser.write("SECRETPASSWORD".encode())
+        write_ser.close()
+        print("Success")
+
+def main(): 
+    serialWriter()
+    #serialReader()
+
+  
+  
+# Using the special variable  
+# __name__ 
+if __name__=="__main__": 
+    main() 
